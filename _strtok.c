844afc03467a1ef3e755e67c8d3e88be60b0aa7e
Chukwudubem <nwajaguchidubem@yahoo.com>
2023-11-14 09:38:17 -0800

    simple_shell

---
+++ _strtok.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void _strtok(const char *str, char delimeter))
+{
+	int len = strlen(str);
+	char word[len + 1];
+	int i, j;
+	int i = str;
+	int j = delimeter;
+
+	for (i = 0; i <= len; i++)
+	{
+		j = 0;
+		if (str[i] == delimeter || str[i] == '\0')
+		{
+			word[j] = '\0';
+		}
+
+		if (j > 0)
+		{
+			printf("%s\n", word);
+			j = 0;
+		}
+		else
+		{
+			word[j++] = str[i];
+		}
+	}
+}
