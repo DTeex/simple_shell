b0626ba99d313e2c6d472f0d8f1a5c4d18fb6142
Chukwudubem <nwajaguchidubem@yahoo.com>
2023-11-14 12:58:55 -0800

    simple_shell

--- _strtok.c
+++ _strtok.c
@@ -1,31 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "shell.h"
 
-int main(void _strtok(const char *str, char delimeter))
+/**
+* cstm_strtok - this is a custom function that tokenizes a string
+* it works based on the delimiter of the string to be tokenized.
+* @str: string to be tokenized which is the first argument.
+* @delim: this represents the delimiter ofthe string in what pattern
+* to be splitted.
+* Return: tokenized string on success, else NULL.
+*/
+
+char *cstm_strtok(char *str, const char *delim);
+
+char *cstm_strtok(char *str, const char *delim)
 {
-	int len = strlen(str);
-	char word[len + 1];
-	int i, j;
-	int i = str;
-	int j = delimeter;
+	static char *next_token;
 
-	for (i = 0; i <= len; i++)
+	char *new_token;
+
+	if (str != NULL)
 	{
-		j = 0;
-		if (str[i] == delimeter || str[i] == '\0')
-		{
-			word[j] = '\0';
-		}
-
-		if (j > 0)
-		{
-			printf("%s\n", word);
-			j = 0;
-		}
-		else
-		{
-			word[j++] = str[i];
-		}
+		/*Updates the next value of the string*/
+		next_token = str;
 	}
+
+	/*Check for the end of tokens in the string*/
+	if (next_token == NULL || *next_token == '\0')
+	{
+		/*End of the string null is returned*/
+		return (NULL);
+	}
+
+	while (*next_token != '\0' && strchr(delim, *next_token) != NULL)
+	{
+		next_token++;
+	}
+
+	/*check for end of tokens*/
+
+	if (*next_token == '\0')
+	{
+		return (NULL);
+	}
+
+	new_token = next_token;
+
+	while (*next_token != '\0' && strchr(delim, *next_token) == NULL)
+	{
+		next_token += 1;
+	}
+
+	if (*next_token != '\0')
+	{
+		*next_token = '\0';
+
+		next_token += 1;
+	}
+
+	/*All strings has been tokenized completely */
+	return (new_token);
 }
--- getenv.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void main(void)
-{
-	char *homevalue = getenv("HOME");
-
-	printf("%s\n", homevalue);
-}
